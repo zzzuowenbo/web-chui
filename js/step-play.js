8080bfbbfe2b6567a99de6e2ab3c8747a15ed346
zzzuowenbo <2895732099@qq.com>
2019-06-10 09:12:45 +0800

    add js

---
+++ js/step-play.js
@@ -0,0 +1,32 @@
+//点击切换图片轮播图
+isCoursel();
+function isCoursel(){
+	new Coursel({
+		id:"box",
+		width:1349,
+		height:962,
+		img:["./image/zwb-z1.png","./image/zwb-z2.png","./image/zwb-z3.png","./image/zwb-z4.png","./image/zwb-z5.png","./image/zwb-z6.png","./image/zwb-z7.png","./image/zwb-z8.png"],
+		playtime:1000
+	});
+}
+
+//购物车效果
+isCart();
+function isCart(){
+	var oCart = document.getElementById('cart');
+	var oHeaderItem = document.getElementById('header-item1');
+	var oIcon = document.querySelector('.header-item1 .box');
+	oHeaderItem.onmouseenter = function(){
+		oIcon.style.color = "#e9eaee";
+		animate(oCart,{height:320},true,function(){
+			oCart.style.display = "block";
+		});
+	}
+	oHeaderItem.onmouseleave = function(){
+		oIcon.style.color = "#9093a7";
+		animate(oCart,{height:0},true,function(){
+			oCart.style.display = "none";
+		});		
+	}
+}
+
--- js/zwb-search.js
+++ js/zwb-search.js
@@ -1,13 +1,12 @@
 // 搜索下拉框开始
 var oSelect = document.getElementById('select');
 var oClick = document.getElementById('click');
-console.log(oClick);
+// console.log(oClick);
 oClick.onclick = function(){
-	if(oSelect.style.display == "block"){
-		oSelect.style.display = "none";
-	}else{
-		oSelect.style.display = "block";
-    }
+ 	oSelect.style.display = "block";
+}
+oSelect.onmouseleave = function(){
+	oSelect.style.display = "none";
 }
 // 搜索下拉框结束
 
@@ -46,3 +45,46 @@ oFrame.onclick = function(){
     }
 }
 // 选择主题分类下拉框结束
+
+//下拉版块效果
+isCoursel();
+function isCoursel(){
+	var oBd = document.getElementById('bd');
+	var oArea = document.getElementById('area');
+	oBd.onmouseenter = function(){
+		animate(oArea,{height:536},true,function(){
+			oArea.style.display = "block";
+		});
+	}
+	oBd.onmouseleave = function(){
+		animate(oArea,{height:0},true,function(){
+			oArea.style.display = "none";
+		});
+	}
+}
+
+//选择相应按钮
+isBtn();
+function isBtn(){
+	var aPageItem = document.querySelectorAll('.content .page-list .page-item');
+	for(var i=0;i<aPageItem.length;i++){
+		aPageItem[i].index = i;
+		aPageItem[i].onclick = function(){
+			for(var j=0;j<aPageItem.length;j++){
+				aPageItem[j].className = "page-item";
+			}
+			this.className = "page-item active";
+		}		
+	} 
+}
+
+//验证码检验
+isCheck();
+function isCheck(){
+	var oVerify = document.getElementById('intext');
+    oVerify.onkeydown = function(ev){
+    	if(oVerify.value.length > 5 && ev.keyCode != 8 && ev.keyCode != 37 && ev.keyCode != 39){
+        	return false;
+        }
+    }
+}
