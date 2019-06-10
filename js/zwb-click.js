f14c1901df8d8f34afd271768a627a2e7a866325
zzzuowenbo <2895732099@qq.com>
2019-06-10 09:32:34 +0800

    add zjs

--- js/zwb-click.js
+++ js/zwb-click.js
@@ -1,5 +1,4 @@
 // 点击切换图片开始
-<<<<<<< HEAD
 	function Coursel(options){
 		this.box  = document.getElementById(options.id);
 		this.width = options.width;
@@ -128,56 +127,6 @@
 		//鼠标移出重新开始动画
 		this.box.onmouseout = function(){
 			timer = setInterval(_this.oRightBtn.onclick,_this.playtime);
-=======
-/*var oDot = document.querySelectorAll('#ul1 li');
-for(var i=0;i<oDot.length;i++){
-	oDot[i].onclick = function(){
-		for(var j=0;j<oDot.length;j++){
-				oDot[j].className = "";
-		} 
-		this.className = "active";
-	} 
-}*/
-    function tab(){
-    	for(var i=0;i<aImg.length;i++){
-			aImg[i].style.zIndex = 0;
-			aBtns[i].className = "";
-			// aImg[i].style.display = "none";
-		}
-		aImg[nowIndex].style.zIndex = 99;
-		aBtns[nowIndex].className = "active";
-		// aImg[nowIndex].style.display = "block";
-    }
-
-	var aImg = document.querySelectorAll('.hide li');
-	var oLeft = document.querySelector('.left');
-	var oRight = document.querySelector('.right');
-	var aBtns = document.querySelectorAll('.dot li');
-	var oBox = document.querySelector('.kind');
-
-	var nowIndex = 0;
-	oRight.onclick = function(){
-		nowIndex++;
-		if(nowIndex >= aImg.length){
-			nowIndex = 0;
-		}
-		tab();
-	}
-
-	oLeft.onclick = function(){
-		nowIndex--;
-		if(nowIndex < 0){
-			nowIndex = aImg.length - 1;
-		}
-		tab();
-	}
-
-	for(var j=0;j<aBtns.length;j++){
-		aBtns[j].index = j;
-		aBtns[j].onclick = function(){
-            nowIndex = this.index;
-            tab();
->>>>>>> c025cbe7e4d5293c195d70c294240b4409b9a217
 		}
 	}
 // 点击切换图片结束
